2f5c83b4226bad6b2f1f57bbefd380cc938a4512
HyeongSikkk <2674625@naver.com>
2024-02-13 02:39:35 +0900

    sudoku

--- sudoku.py
+++ sudoku.py
@@ -16,13 +16,23 @@ for row_idx, line in enumerate(array) :
             target_list.append((row_idx, col_idx))
 
 
+class TestError(Exception) :
+    pass
+
 class Solutioner :
-    def __init__(self, array, target_list) :
+    def __init__(self, array) :
         self.array = array
-        self.target_list = target_list
-        self.idx = 0
+        self.target_list = self.get_target_list()
         self.boxes = self.box_maker()
-        self.bundel_boxes = self.bundle_maker()
+        self.bundle_boxes = self.bundle_maker()
+    
+    def get_target_list(self) :
+        target_list = []
+        for row_idx, line in enumerate(self.array) :
+            for col_idx, ea in enumerate(line) :
+                if ea == "0" :
+                    target_list.append((row_idx, col_idx))
+        return target_list
     
     def box_maker(self) :
         boxes = []
@@ -31,71 +41,38 @@ class Solutioner :
             box = Box(row=row,col=col)
             box.mining(self.array)
             boxes.append(box)
-        
-        for idx, box in enumerate(boxes) :
-            # 첫 박스
-            if idx == 0 :
-                box.backward = boxes[idx+1]
-            
-            # 마지막 박스
-            elif idx == len(boxes)-1 :
-                box.forward = boxes[idx-1]
-            
-            # 그 외 박스
-            else :
-                box.forward = boxes[idx-1]
-                box.backward = boxes[idx +1]
         return boxes
     
     
     def bundle_maker(self) :
-        bundle_boxes = []
-        
-        for bundle in range(1, 10) : # 1, 2, 3, ... , 9
-            bundle_boxes.append(Bundle_box(bundle, self.boxes))
-        
+        bundle_boxes = [Bundle_box(bundle, self.boxes) for bundle in range(1, 10)]
         return bundle_boxes
     
-    
-    def re_bundle_maker(self) :
-        bundle_boxes = []
-        
-        for bundle in range(1, 10) : # 1, 2, 3, ..., 9
-            bundle_boxes.append(Bundle_box(bundle, self.boxes))
-        
-        self.bundel_boxes = bundle_boxes
-        
-        
+
     def re_mining(self, row, col, bundle) :
         for box in self.boxes :
             if box.row == row or box.col == col or box.bundle == bundle :
                 box.mining(self.array)
                 
-    def select_one_cases_box(self, re = True) :
+                
+    def select_one_cases_box(self) :
         # 숫자 경우의 수가 하나인 박스들 탐색
-        good_box = []
         for idx, box in enumerate(self.boxes) :
             if len(box.cases) == 1 :
-                good_box.append(box)
-                if re :
-                    self.target_list.remove((box.row, box.col))
-                    self.boxes.remove(box)
-        
-        if len(good_box) != 0 :
-            # 배열에 값 변경
-            for box in good_box :
+                self.target_list.remove((box.row, box.col))
+                self.boxes.remove(box)
+                # 배열에 값 변경
                 self.array[box.row][box.col] = box.cases[0]
                 self.re_mining(box.row, box.col, box.bundle)
-                
+
             # 지역 객체 재정비
-            if re :
-                self.re_bundle_maker()
+            self.bundle_boxes = self.bundle_maker()
             
 
     # 번들에서 가능한 경우 탐색 후, 하나인 경우를 발견하면 해당 값 부여
-    def select_one_cases_box_in_bundle(self, re = True) :
+    def select_one_cases_box_in_bundle(self) :
         # 각 번들 꺼내기
-        for bundle_box in self.bundel_boxes :
+        for bundle_box in self.bundle_boxes :
             
             # 번들에서 박스들 꺼내서 한 값만 가능한 경우 탐색
             numbers = {}
@@ -116,6 +93,7 @@ class Solutioner :
                 for key, value in numbers.items() :
                     if value == 1 :
                         one_cases.append(key)
+                        break
                 
                 # 빈도 수가 1인 값을 가진 박스에 해당 값 주입하기
                 for one in one_cases :
@@ -125,56 +103,76 @@ class Solutioner :
                             self.target_list.remove((box.row, box.col))
                             self.boxes.remove(box)
                             self.re_mining(box.row, box.col, box.bundle)
+                            break
         
-        if re :
-            self.re_bundle_maker()
+                self.bundle_boxes = self.bundle_maker()
                 
-                    
         
     def solve(self) :
-        while True :
+        while len(self.target_list) != 0 :
             before_array = [row[:] for row in self.array]
             self.select_one_cases_box()
             self.select_one_cases_box_in_bundle()
             if before_array == self.array :
-                if len(target_list) != 0 :
+                if len(self.target_list) != 0 :
                     self.reducing()
+                    self.solve()
                 break
-        
-        
     
     
-    def reducing(self) :
-        # 제일 적은 숫자 경우의 수를 가진 박스 추출
-        select_box = self.find_box()
-        good_numbers = [] # 틀리지 않았다 판단되는 경우를 담는 리스트
+    def reducing(self, min = True) :
+        repair_array = [row[:] for row in self.array]
+        repair_target_list = [i for i in self.target_list]
+        repair_boxes = [i for i in self.boxes]
+        repair_bundle_boxes = [i for i in self.bundle_boxes]
         
-        for num in select_box.cases :
-            if len(self.target_list) == 0 :
-                break
-            result = self.do_test(num, select_box) # {'result' : boolean, 'num' : str(int)}
-            if result['result'] :
-                good_numbers.append(num)
-        
-        if len(good_numbers) == 1 :
-            if len(self.target_list) != 0 :
-                self.choice(good_numbers[0], select_box)
-                self.solve()
-        else :
-            if len(self.target_list) != 0 :
-                select_box = self.find_box(min = False)
-                good_numbers = []
-                for num in select_box.cases :
-                    if len(self.target_list) == 0 :
-                        break
-                    result = self.do_test(num, select_box) # {'result' : boolean, 'num' : str(int)}
-                    if result['result'] :
-                        good_numbers.append(num)
+        if min : # 처음 호출된 경우
+            # 제일 적은 숫자 경우의 수를 가진 박스 추출
+            select_box = self.find_box()
+            good_numbers = [] # 틀리지 않았다 판단되는 경우를 담는 리스트
+            bad_numbers = [] # 틀린 경우를 담는 리스트
 
-                if len(good_numbers) == 1 :
-                    if len(self.target_list) != 0 :
-                        self.choice(good_numbers[0], select_box)
-                        self.solve()
+            for num in select_box.cases :
+                if len(self.target_list) == 0 :
+                    break
+                result = self.do_test(num, select_box) # {'result' : boolean, 'num' : str(int)}
+                if result['result'] :
+                    good_numbers.append(num)
+                else :
+                    bad_numbers.append(num)
+
+            if len(good_numbers) == 1 :
+                self.choice(good_numbers[0], select_box)
+
+            elif len(good_numbers) == 0 :
+                raise TestError('TestError, zero case')
+            elif len(bad_numbers) >= 1 and len(good_numbers) != 0 : # 테스팅 결과가 유의미한 경우
+                select_box.wrong_cases = bad_numbers
+                self.re_mining(select_box.row, select_box.col, select_box.bundle)
+
+            else : # 틀리지 않은 경우들이 없을경우 다른 박스로 테스트
+                self.reducing(min = False)
+                
+        else : # 첫 호출에서 풀이가 불가능 했을 경우
+            select_box = self.find_box(min = False)
+            good_numbers = [] # 틀리지 않았다 판단되는 경우를 담는 리스트
+            bad_numbers = [] # 틀린 경우를 담는 리스트
+
+            for num in select_box.cases :
+                if len(self.target_list) == 0 :
+                    break
+                result = self.do_test(num, select_box) # {'result' : boolean, 'num' : str(int)}
+                if result['result'] :
+                    good_numbers.append(num)
+                else :
+                    bad_numbers.append(num)
+
+            if len(good_numbers) == 1 :
+                self.choice(good_numbers[0], select_box)
+
+            elif len(bad_numbers) >= 1 : # 테스팅 결과가 유의미한 경우
+                select_box.wrong_cases = bad_numbers
+                self.re_mining(select_box.row, select_box.col, select_box.bundle)
 
         
     # 제일 적은 숫자 경우의 수를 가진 박스 탐색
@@ -198,11 +196,11 @@ class Solutioner :
     
     # 박스에 가능한 경우의 값을 넣어보기
     def do_test(self, num, select_box) :
-        print(f"testing\n    {select_box.row, select_box.col} : {num}")
         # 테스트 전의 배열, 테스트 결과에 실패 시에 원상복구 할 목적
         repair_array = [row[:] for row in self.array]
         repair_target_list = [i for i in self.target_list]
         repair_boxes = [i for i in self.boxes]
+        repair_bundle_boxes = [i for i in self.bundle_boxes]
         
         # 테스트 시작
         self.choice(num, select_box)
@@ -211,21 +209,32 @@ class Solutioner :
             self.array = repair_array
             self.target_list = repair_target_list
             self.boxes = repair_boxes
+            self.bundle_boxes = repair_bundle_boxes
             return {'result' : True, 'num' : num}
         except :
             self.array = repair_array
             self.target_list = repair_target_list
             self.boxes = repair_boxes
+            self.bundle_boxes = repair_bundle_boxes
             return {'result' : False, 'num' : num}
         
     
     def choice(self, num, box) :
-        print(f"choice\n    {box.row, box.col} : {num}")
         self.array[box.row][box.col] = num
         self.target_list.remove((box.row, box.col))
         self.boxes.remove(box)
         self.re_mining(box.row, box.col, box.bundle)
-        self.re_bundle_maker()
+        self.bundle_boxes = self.bundle_maker()
+    
+    def repair(self) :
+        self.target_list = self.get_target_list()
+        self.boxes = self.box_maker()
+        self.bundle_boxes = self.bundle_maker()
+        
+    def do(self) :
+        while len(self.target_list) != 0 :
+            self.solve()
+            self.repair()
         
     
         
@@ -245,6 +254,7 @@ class Box :
     def __init__(self, row, col) :
         self.row = row
         self.col = col
+        self.wrong_cases = []
         self.bundle = get_bundle(row, col)
     
     def mining(self, array) :
@@ -295,7 +305,17 @@ class Box :
                 if target not in target_numbers :
                     target_numbers.append(target)
         
-        self.cases = target_numbers
+        if len(target_numbers) == 0 :
+            raise TestError(f'TestError, {row_idx, col_idx}')
+        
+        if len(self.wrong_cases) == 0 :
+            self.cases = target_numbers
+        else :
+            for target in target_numbers :
+                if target in self.wrong_cases :
+                    target_numbers.remove(target)
+            
+            self.cases = target_numbers
 
 def get_bundle(row_idx, col_idx) :
     if row_idx <= 2 :
@@ -315,51 +335,9 @@ def get_bundle(row_idx, col_idx) :
     return row_positions+3*col_positions
 
 
-s = Solutioner(array, target_list)
+s = Solutioner(array)
 
-s.solve()
-def check(array) :
-    # 가로, 세로 테스트
-    for row_idx in range(9) :
-        for col_idx in range(9) :
-            rows = []
-            cols = []
-            
-            for row in array[row_idx] :
-                if not row in rows :
-                    rows.append(row)
-                else :
-                    print('error')
-                    print(row_idx, col_idx)
-                    return
-
-            for col in array :
-                if not col[col_idx] in cols :
-                    cols.append(col[col_idx])
-                else :
-                    print('error')
-                    print(row_idx, col_idx)
-                    return
-                
-    # bundel test
-    row_bundles = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    col_bundles = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    for rows in row_bundles :
-        for cols in col_bundles :
-            numbers = []
-            for row in rows :
-                for col in cols :
-                    num = array[row][col]
-                    if not num in numbers :
-                        numbers.append(num)
-                    else :
-                        print('error')
-                        return
-    return 'perfect'
-
-
-
-print(check(s.array))
+s.do()
 text = ''
 for line in s.array :
     text += ' '.join(line)+'\n'
