14d327248bffc0bd9261f115dd7bb806f1b64fd7
HyeongSikkk <2674625@naver.com>
2024-04-07 18:08:21 +0900

    2178

--- baekjoon/2178/2178.py
+++ baekjoon/2178/2178.py
@@ -1,23 +1,50 @@
-rows, cols = list(map(int, input().strip().split(' ')))
+rows, cols = map(int, input().strip().split(' '))
 maps = []
+target_list = []
+camed = set()
 for _ in range(rows) :
     maps.append(input().strip())
 maps = list(map(list, maps))
-def solver(n, position) :
-    r, c = position
-    if r == rows-1 and c == cols-1 :
-        print(n)
-        exit()
-    maps[r][c] = '0'
-    fore_casts = [
-        (1, 0),
-        (-1, 0),
-        (0, 1),
-        (0, -1),
+fore_casts = [
+    (0, 1),
+    (1, 0),
+    (-1, 0),
+    (0, -1),
     ]
-    for fore_cast in fore_casts :
-        fr, fc = fore_cast
-        if 0 <= r+fr < rows and 0 <= c + fc < cols :
-            if maps[r+fr][c+fc] == '1' :
-                solver(n+1, (r+fr, c+fc))
-solver(1, (0, 0))
+n = 0
+now = [(0, 0)]
+
+# def map_print(now) :
+    
+#     for r_idx, row in enumerate(maps) :
+#         text = ''
+#         for c_idx, ea in enumerate(row) :
+#             if (r_idx, c_idx) in now :
+#                 text += 'F'
+#             else :
+#                 text += ea
+#         print(text)
+#     print()
+
+camed.add(now[0])
+while True :
+    find = False
+    n += 1
+    now += target_list
+    target_list = []
+#    map_print(now)
+    for position in now :
+        r, c = position
+        if r == rows-1 and c == cols -1 :
+            find = True
+            break
+        for fore_cast in fore_casts :
+            fr, fc = fore_cast
+            if 0 <= r+fr < rows and 0 <= c+fc < cols and (r+fr, c+fc) not in camed:
+                if maps[r+fr][c+fc] == '1' :
+                    target_list.append((r+fr, c+fc))
+                    camed.add((r+fr, c+fc))
+    now = []
+    if find :
+        print(n)
+        break
