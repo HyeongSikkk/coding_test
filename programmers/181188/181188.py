3f15614679b9d9a393bd29f3a2ed657e1e4408ad
HyeongSikkk <2674625@naver.com>
2024-09-10 17:05:06 +0900

    181188

--- programmers/181188/181188.py
+++ programmers/181188/181188.py
@@ -1,48 +1,11 @@
 def solution(targets) :
-    class Node :
-        def __init__(self, widths) :
-            self.start, self.end = widths
-            self.left = None
-            self.middle = None
-            self.right = None
-
-    class TargetTree :
-        def __init__(self) :
-            self.head = None
-            self.cnt = 0
-
-        def link_target(self, widths) :
-            if self.head is None :
-                self.head = Node(widths)
-                self.cnt += 1
-
-            else :
-                nodes = [self.head]
-                while nodes :
-                    n = nodes.pop()
-                    # 범위가 통하는 경우
-                    if n.start < widths[1] and n.start >= widths[0] :
-                        if n.middle is None :
-                            n.middle = [Node(widths)]
-                        else :
-                            n.middle.append(Node(widths))
-
-                    # 값이 더 큰 경우
-                    elif n.end <= widths[0] :
-                        if n.right is None :
-                            n.right = Node(widths)
-                            self.cnt += 1
-                        else :
-                            nodes.append(n.right)
-                    # 값이 더 작은 경우
-                    else :
-                        if n.left is None :
-                            n.left = Node(widths)
-                            self.cnt += 1
-                        else :
-                            nodes.append(n.left)
-    targets.sort(key = lambda x : x[1] - x[0])
-    t = TargetTree()
-    for target in targets :
-        t.link_target(target)
-    return t.cnt
+    targets.sort(lambda x : x[1])
+    
+    min_b = targets[0][1] # 처음의 끝 값
+    ea = 1
+    for target in targets[1:] :
+        a, b = target
+        if min_b < a :
+            min_b = b
+            ea += 1
+    return ea
